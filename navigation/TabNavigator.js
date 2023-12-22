5d7f27f8aaa87fcd6414aa3e90aa912fe00732e0
V-Tummala <129685407+V-Tummala@users.noreply.github.com>
2023-12-22 13:00:12 +0000

    Update TabNavigator.js

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -38,14 +38,6 @@ const BottomTabNavigator = () => {
 };
 
 const styles = StyleSheet.create({
-    bottomTabStyle: {
-        backgroundColor: "#2f345d",
-        height: "8%",
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        overflow: "hidden",
-        position: "absolute"
-    },
     icons: {
         width: RFValue(30),
         height: RFValue(30)
